687a258a91ffaaab6e388d380245455051caf5d7
Marcy Sutton <holla@marcysutton.com>
2021-10-15 20:59:17 -0700

    Update exercise 1 to prep for exercise 2

--- exercise1-keyboard/after/script.js
+++ exercise1-keyboard/after/script.js
@@ -2,13 +2,16 @@ function load() {
     var menuItems = Array.from(document.querySelectorAll('.megamenu-navitem'))
     var submenus = Array.from(document.querySelectorAll('.megamenu-section'))
 
+    function resetSubmenus(event) {
+        var activeSubmenu = document.querySelector('.megamenu-section.active')
+        if (activeSubmenu && !activeSubmenu.contains(event.target)) {
+            activeSubmenu.classList.remove('active')
+        }
+    }
     menuItems.forEach((menuItem, index) => {
         let parentElement = menuItem.parentNode
         menuItem.addEventListener('click', function(event) {
-            // reset all parent submenus
-            submenus.forEach((submenu, index) => {
-                submenu.classList.remove('active')
-            })
+            resetSubmenus(event)
 
             // Add active menu for the item clicked
             if(!parentElement.classList.contains('active')) {
@@ -24,6 +27,9 @@ function load() {
                 submenu.querySelector('.megamenu-navitem').focus()
             }
         })
+    })
+    document.addEventListener('click', (event) => {
+        resetSubmenus(event)
     })
 }
 
