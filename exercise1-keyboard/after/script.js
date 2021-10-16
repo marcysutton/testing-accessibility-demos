01529e774831d0c992267ac9beb956c1983b374d
Marcy Sutton <holla@marcysutton.com>
2021-10-15 18:44:30 -0700

    Updates for exercise 1: after

--- exercise1-keyboard/after/script.js
+++ exercise1-keyboard/after/script.js
@@ -1,22 +1,30 @@
 function load() {
-    var header = document.querySelector('#header')
-    var megaMenu = document.querySelector('#header-megamenu')
+    var menuItems = Array.from(document.querySelectorAll('.megamenu-navitem'))
+    var submenus = Array.from(document.querySelectorAll('.megamenu-section'))
 
-    // megaMenu.addEventListener('mouseover', function(event) {
-    //     if (event.target.classList.contains('megamenu-section')){
-    //         console.log(event.target.classList)
-    //         if(!event.target.classList.contains('active')) {
-    //             event.target.classList.add('active')
-    //         }
-    //     }
-    // })
-    // megaMenu.addEventListener('mouseout', function(event) {
-    //     if (event.target.classList.contains('megamenu-section')){
-    //         if(!event.target.classList.contains('active')) {
-    //             event.target.classList.remove('active')
-    //         }
-    //     }
-    // })
+    menuItems.forEach((menuItem, index) => {
+        let parentElement = menuItem.parentNode
+        menuItem.addEventListener('click', function(event) {
+            // reset all parent submenus
+            submenus.forEach((submenu, index) => {
+                submenu.classList.remove('active')
+            })
+
+            // Add active menu for the item clicked
+            if(!parentElement.classList.contains('active')) {
+                parentElement.classList.add('active')
+            }
+        })
+    })
+    submenus.forEach((submenu) => {
+        // Bind escape key to close and handle focus
+        submenu.addEventListener('keyup', (event) => {
+            if (event.key === 'Escape' && submenu.classList.contains('active')) {
+                submenu.classList.remove('active')
+                submenu.querySelector('.megamenu-navitem').focus()
+            }
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', load)
