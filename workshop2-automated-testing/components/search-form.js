707aab6b487c07c74abb4def06d57d1adc86eafa
Marcy Sutton <holla@marcysutton.com>
2022-02-28 18:16:08 -0800

    Fix styling of homepage component

--- workshop2-automated-testing/components/search-form.js
+++ workshop2-automated-testing/components/search-form.js
@@ -3,7 +3,7 @@ import React from "react"
 import ButtonSubmit from "workshop2-components/button-submit"
 
 const SearchForm = () => (
-    <div className="form-wrap layout">
+    <div className="main-search form-wrap layout">
         <div className="form-field">
             <span className="label">Where to?</span>
             <div className="custom-input">
--- workshop4-interactions-mechanics/components/search-form.js
+++ workshop4-interactions-mechanics/components/search-form.js
@@ -3,7 +3,7 @@ import React from "react"
 import ButtonSubmit from "workshop4-components/button-submit"
 
 const SearchForm = () => (
-    <form className="form-wrap layout">
+    <form className="main-search form-wrap layout">
         <div className="form-field">
             <label htmlFor="place-search" className="label">Where to?</label>
             <div className="custom-input">
