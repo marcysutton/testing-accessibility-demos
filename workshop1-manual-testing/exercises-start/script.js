6b29f26a20bcc05c5885a905ca6408efda85fa2a
Marcy Sutton <holla@marcysutton.com>
2022-03-01 16:46:08 -0800

    Reset workshop1 start files

--- workshop1-manual-testing/exercises-start/script.js
+++ workshop1-manual-testing/exercises-start/script.js
@@ -10,29 +10,17 @@ function load() {
     }
 
     menuItems.forEach((menuItem, index) => {
-        let parentElement = menuItem.parentNode.parentNode
+        let parentElement = menuItem.parentNode
         menuItem.addEventListener('click', function(event) {
+            console.log(menuItem)
             resetSubmenus(event)
 
             // Add active menu for the item clicked
             if(!parentElement.classList.contains('active')) {
                 parentElement.classList.add('active')
 
-                menuItem.setAttribute('aria-expanded', 'true')
             } else {
                 parentElement.classList.remove('active')
-
-                menuItem.setAttribute('aria-expanded', 'false')
-            }
-        })
-    })
-
-    submenus.forEach((submenu) => {
-        // Bind escape key to close and handle focus
-        submenu.addEventListener('keyup', (event) => {
-            if (event.key === 'Escape' && submenu.classList.contains('active')) {
-                submenu.classList.remove('active')
-                submenu.querySelector('.megamenu-navitem').focus()
             }
         })
     })
